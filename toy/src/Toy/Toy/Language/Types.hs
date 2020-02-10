01a119009dab33d45c880bbefed0954f0d910594
0xd34df00d <0xd34df00d@gmail.com>
2020-02-10 10:18:24 -0500

    Throw up some definitions for the types

--- toy/src/Toy/Toy/Language/Types.hs
+++ toy/src/Toy/Toy/Language/Types.hs
@@ -1,1 +1,26 @@
 module Toy.Toy.Language.Types where
+
+newtype VarName = VarName { getName :: String } deriving (Eq, Ord, Show)
+
+data BaseTy = TBool | TInt
+  deriving (Eq, Ord, Show)
+
+-- Following liquid-types-08 paper, chapter 5
+
+data RefinementOp = ROpLe | ROpLeq | ROpEq | ROpNEq | ROpGe | ROpGeq deriving (Eq, Ord, Show)
+
+data RefinementArg = RArgZero | RArgVar VarName | RArgVarLen VarName deriving (Eq, Ord, Show)
+
+data BaseRefinement = BR RefinementOp RefinementArg
+  deriving (Eq, Ord, Show)
+
+data Refinement
+  = RTrue
+  | RAnd Refinement Refinement
+  | RBase BaseRefinement
+  deriving (Eq, Ord, Show)
+
+data Ty
+  = TyArrow Ty Ty
+  | TyRefined (Maybe VarName) BaseTy Refinement
+  deriving (Eq, Ord, Show)
