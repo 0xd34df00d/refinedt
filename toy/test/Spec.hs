9f7e0f8ecc43fe0f3dc85b553d02097483299888
0xd34df00d <0xd34df00d@gmail.com>
2020-02-14 11:07:05 -0500

    Start throwing up some tests

--- toy/test/Spec.hs
+++ toy/test/Spec.hs
@@ -1,2 +1,15 @@
+import Data.Void
+import Test.Hspec
+import Text.Megaparsec
+
+import Toy.Language.Parser
+import Toy.Language.Types
+
+parse' :: Parsec Void String a -> String -> Either (ParseErrorBundle String Void) a
+parse' p = parse p ""
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+  describe "Parsing base refined types" $ do
+    it "parses unrefined type" $
+      parse' parseBaseRT "Bool" `shouldBe` Right (RefinedBaseTy TBool trueRefinement)
