36c9fa12430e9090448adcd4cda725c40d04becb
0xd34df00d <0xd34df00d@gmail.com>
2020-02-14 09:52:34 -0500

    Parse variable names (relying on IsList)

--- toy/src/Toy/Language/Parser.hs
+++ toy/src/Toy/Language/Parser.hs
@@ -4,15 +4,19 @@
 
 module Toy.Language.Parser where
 
+import Data.Char
 import Data.Functor
 import Data.String
+import GHC.Exts
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer as ML
 
 import Toy.Language.Types
 
-type ToyMonad e s m = (MonadParsec e s m, Token s ~ Char, IsString (Tokens s))
+type ToyMonad e s m = (MonadParsec e s m,
+                       Token s ~ Char, IsString (Tokens s),
+                       IsList (Tokens s), Item (Tokens s) ~ Char)
 
 tyParser :: ToyMonad e s m => m Ty
 tyParser = undefined
@@ -26,6 +30,9 @@ parseAtomicRefinement = undefined
 parseBaseTy :: ToyMonad e s m => m BaseTy
 parseBaseTy = lstring "Bool" $> TBool
           <|> lstring "Int" $> TInt
+
+parseVarName :: ToyMonad e s m => m VarName
+parseVarName = VarName . toList <$> takeWhile1P (Just "variable") isLetter
 
 lexeme' :: ToyMonad e s m => m a -> m a
 lexeme' = lexeme $ ML.space space1 empty empty
