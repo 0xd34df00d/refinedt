7fc2b9e4958be5d04977f0465a818e302ff044ba
0xd34df00d <0xd34df00d@gmail.com>
2020-03-04 18:10:58 -0500

    Add syntax and parser for declarations

--- toy/src/Toy/Language/Parser/Decl.hs
+++ toy/src/Toy/Language/Parser/Decl.hs
@@ -1,4 +1,17 @@
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
+
 module Toy.Language.Parser.Decl where
 
+import Data.Functor
+
+import Toy.Language.Parser.Ty
 import Toy.Language.Parser.Util
-import Toy.Language.Syntax.Types
+import Toy.Language.Syntax.Decl
+
+funDecl :: ToyMonad e s m => m FunDecl
+funDecl = do
+  funName <- lexeme' identifier
+  void $ lsymbol ":"
+  funTy <- ty
+  pure FunDecl { .. }
--- toy/src/Toy/Language/Syntax/Decl.hs
+++ toy/src/Toy/Language/Syntax/Decl.hs
@@ -1,2 +1,8 @@
 module Toy.Language.Syntax.Decl where
 
+import Toy.Language.Syntax.Types
+
+data FunDecl = FunDecl
+  { funName :: String
+  , funTy :: Ty
+  } deriving (Eq, Ord, Show)
