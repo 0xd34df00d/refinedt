a9ea9cada87be640c396c9663f2f37dbd79cdb0b
0xd34df00d <0xd34df00d@gmail.com>
2020-03-07 18:49:08 -0500

    Stub compiler tests

---
+++ toy/test/CompilerSpec.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleContexts, GADTs #-}
+
+module CompilerSpec(spec) where
+
+import Control.Monad.IO.Class
+import Data.Either
+import Data.Void
+import Test.Hspec
+import Text.Megaparsec hiding (State)
+import Text.SExpression
+
+import Idris.IdeModeClient
+import Toy.Language.Compiler
+import Toy.Language.Parser.Decl
+import Toy.Language.Syntax.Decl
+
+parse' :: String -> IO FunDecl
+parse' str = do
+  parsed `shouldSatisfy` isRight
+  pure $ either (error . show) id parsed
+  where
+   parsed = runParser (funDecl <* eof) "" str :: Either (ParseErrorBundle String Void) FunDecl
+
+isOkReply :: SExpr -> Bool
+isOkReply (List [Atom ":return", List (Atom ":ok" : _), _]) = True
+isOkReply _ = False
+
+spec :: Spec
+spec =
+  describe "Basic smoke tests" $
+    it "Parses base types" $ do
+      parsed <- parse' "someBool : Bool"
+      withIdris $ do
+        sendCommand $ typeCheck $ compileFunDecl parsed
+        reply <- readReply
+        liftIO $ reply `shouldSatisfy` isOkReply
