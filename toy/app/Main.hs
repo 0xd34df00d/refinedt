a54c91517d39b7d6656f588606d6a1535ced6aa9
0xd34df00d <0xd34df00d@gmail.com>
2020-03-23 11:30:18 -0400

    Throw up a `main` with some dummy stuff for testing

--- toy/app/Main.hs
+++ toy/app/Main.hs
@@ -1,4 +1,21 @@
 module Main where
 
+import Data.Void
+import System.Environment
+import Text.Megaparsec hiding (State)
+
+import Toy.Language.Compiler
+import Toy.Language.Parser.Decl
+import Toy.Language.Syntax.Decl
+
+parseFunDecl :: String -> FunDecl
+parseFunDecl str = either (error . show) id parsed
+  where
+   parsed = runParser (funDecl <* eof) "" str :: Either (ParseErrorBundle String Void) FunDecl
+
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  case args of
+       ["compileTy", funDeclStr] -> putStrLn $ compileFunDecl $ parseFunDecl funDeclStr
+       _ -> putStrLn "Unrecognized arguments"
