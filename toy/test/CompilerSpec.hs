5cc17e17266e76bc6ee27d0467d9fc8e2580d261
0xd34df00d <0xd34df00d@gmail.com>
2020-03-27 13:25:07 -0400

    Generating base refinement values

--- toy/test/CompilerSpec.hs
+++ toy/test/CompilerSpec.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE FlexibleContexts, GADTs #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
 
 module CompilerSpec(spec) where
 
 import Control.Monad.IO.Class
 import Control.Monad.Loops
+import Control.Monad.State.Strict
 import Data.Either
+import Data.List.Extra
 import Data.Void
+import QuickCheck.GenT
 import Test.Hspec
 import Text.Megaparsec hiding (State)
 import Text.SExpression
@@ -14,6 +18,7 @@ import Idris.IdeModeClient
 import Toy.Language.Compiler
 import Toy.Language.Parser.Decl
 import Toy.Language.Syntax.Decl
+import Toy.Language.Syntax.Types
 
 parse' :: String -> IO FunDecl
 parse' str = do
@@ -48,3 +53,27 @@ spec = beforeAll startIdris $ afterAll stopIdris $
       it "refinements" $ checkIdris "someFun : { ν : Int | ν > 0 } -> Bool"
       it "pi-bound vars" $ checkIdris "someFun : (x : Int) -> Bool"
       it "pi-bound vars and refinements" $ checkIdris "someFun : (x : { ν : Int | ν > 0 }) -> Bool"
+
+instance Arbitrary Ty where
+  arbitrary = (`evalState` []) <$> runGenT genTy
+
+genTy :: MonadState [(VarName, BaseTy)] m => GenT m Ty
+genTy = {-frequency [ (3, TyArrow <$> genTyArrow)
+                  , (2, TyBase <$> genTyBase)
+                  ]-} TyBase <$> genTyBase
+  where
+    genTyArrow = undefined
+    genTyBase = RefinedBaseTy <$> elements enumerate <*> genRefinement
+    genRefinement = Refinement <$> listOf genAtomicRefinement
+    genAtomicRefinement = AR <$> genRefinementOp <*> genRefinementArg
+    genRefinementOp = elements enumerate
+    genRefinementArg = do
+      vars <- get
+      oneof $ pure RArgZero
+            : [ pure $ RArgVar var | (var, TInt) <- vars ]
+           <> [ pure $ RArgVarLen var | (var, TIntList) <- vars ]
+
+instance MonadState s m => MonadState s (GenT m) where
+  state = lift . state
+  get = lift get
+  put = lift . put
