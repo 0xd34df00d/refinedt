864f8f4f283ec08e2d8c0af17159f3a849c3bff3
0xd34df00d <0xd34df00d@gmail.com>
2020-04-01 12:41:17 -0400

    Start working on the definitions/expressions parsing

--- toy/src/Toy/Language/Syntax/Decl.hs
+++ toy/src/Toy/Language/Syntax/Decl.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module Toy.Language.Syntax.Decl where
 
 import Toy.Language.Syntax.Types
@@ -6,3 +8,19 @@ data FunDecl = FunDecl
   { funName :: String
   , funTy :: Ty
   } deriving (Eq, Ord, Show)
+
+data FunDef = FunDef
+  { funName :: String
+  , funArgs :: [VarName]
+  , funBody :: Expr
+  } deriving (Eq, Ord, Show)
+
+data Op = OpPlus | OpMinus | OpGt | OpLt deriving (Eq, Ord, Show)
+
+data Expr
+  = EName VarName
+  | EInt Int
+  | EOperator Op
+  | EApp Expr Expr
+  | EIfThenElse { eif :: Expr, ethen :: Expr, eelse :: Expr }
+  deriving (Eq, Ord, Show)
