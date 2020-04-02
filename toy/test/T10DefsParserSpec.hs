c57277da1335df43420f0241696979e46999a6f9
0xd34df00d <0xd34df00d@gmail.com>
2020-04-02 12:41:17 -0400

    Test application gets parsed

--- toy/test/T10DefsParserSpec.hs
+++ toy/test/T10DefsParserSpec.hs
@@ -13,3 +13,4 @@ spec :: Spec
 spec = do
   describe "Parsing basic fundefs" $ let p = parse' funDef in do
     it "parses identity function" $ p "id x = x" ~~> FunDef "id" ["x"] (TName "x")
+    it "parses application" $ p "dot x y = x y" ~~> FunDef "dot" ["x", "y"] (TName "x" `TApp` TName "y")
