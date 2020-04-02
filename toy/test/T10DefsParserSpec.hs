ffcd119e89969c2c7bc9071dd489004b40cc050c
0xd34df00d <0xd34df00d@gmail.com>
2020-04-02 12:59:46 -0400

    Test operators parsing

--- toy/test/T10DefsParserSpec.hs
+++ toy/test/T10DefsParserSpec.hs
@@ -10,10 +10,11 @@ import Toy.Language.Syntax.Decl
 import TestUtils
 
 spec :: Spec
-spec = do
+spec =
   describe "Parsing basic fundefs" $ let p = parse' funDef in do
     it "parses identity function" $ p "id x = x" ~~> FunDef "id" ["x"] nx
     it "parses application" $ p "dot x y = x y" ~~> FunDef "dot" ["x", "y"] (nx `TApp` ny)
+    it "parses ops" $ p "f x y = x > y" ~~> FunDef "f" ["x", "y"] (TBinOp nx BinOpGt ny)
 
 nx, ny :: Term
 nx = TName "x"
