5a09946a5ad24ad7c80231502927cd1c0f208a15
0xd34df00d <0xd34df00d@gmail.com>
2020-04-02 13:26:50 -0400

    A tad nicer formatting

--- toy/test/T10DefsParserSpec.hs
+++ toy/test/T10DefsParserSpec.hs
@@ -12,9 +12,12 @@ import TestUtils
 spec :: Spec
 spec =
   describe "Parsing basic fundefs" $ let p = parse' funDef in do
-    it "parses identity function" $ p "id x = x" ~~> FunDef "id" ["x"] nx
-    it "parses application" $ p "dot x y = x y" ~~> FunDef "dot" ["x", "y"] (nx `TApp` ny)
-    it "parses ops" $ p "f x y = x > y" ~~> FunDef "f" ["x", "y"] (TBinOp nx BinOpGt ny)
+    it "parses identity function" $
+      p "id x = x" ~~> FunDef "id" ["x"] nx
+    it "parses application" $
+      p "dot x y = x y" ~~> FunDef "dot" ["x", "y"] (nx `TApp` ny)
+    it "parses ops" $
+      p "f x y = x > y" ~~> FunDef "f" ["x", "y"] (TBinOp nx BinOpGt ny)
 
 nx, ny :: Term
 nx = TName "x"
