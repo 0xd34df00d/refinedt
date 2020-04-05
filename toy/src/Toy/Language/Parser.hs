003195ed3846966f8be212ea9ce47b118f586d90
0xd34df00d <0xd34df00d@gmail.com>
2020-04-05 13:03:56 -0400

    Added a function decl/def pair parser

---
+++ toy/src/Toy/Language/Parser.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Toy.Language.Parser where
+
+import Data.Functor
+import Data.String
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+import Toy.Language.Parser.Decl
+import Toy.Language.Parser.Util
+import Toy.Language.Syntax.Decls
+
+fun :: ToyMonad e s m => m (FunSig, FunDef)
+fun = do
+  sig@FunSig { .. } <- funDecl <* eol
+  let nameParser = string (fromString funName) $> funName
+  def <- funDefNamed nameParser <* optional eol
+  pure (sig, def)
