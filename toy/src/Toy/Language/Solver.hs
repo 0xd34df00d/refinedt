12f2aec55302cd8a145dfc91cafec84b0c7cd1c8
0xd34df00d <0xd34df00d@gmail.com>
2020-04-07 01:02:39 -0400

    More type-safe Z3 var name passing

--- toy/src/Toy/Language/Solver.hs
+++ toy/src/Toy/Language/Solver.hs
@@ -25,25 +25,27 @@ solve _ sig def = evalZ3 $ mkScript arg2type resType (funBody def)
 
 type ArgTypes = HM.HashMap VarName Ty
 
+newtype Z3VarName = Z3VarName { getZ3VarName :: AST }
+
 -- This expects that the pi-binders names in the type are equal to the argument names in the definition.
 -- TODO explicitly check for this.
 mkScript :: ArgTypes -> RefinedBaseTy -> Term -> Z3 SolveRes
 mkScript args target term = do
-  z3vars <- HM.fromList <$> mapM sequence [ (var, mkFreshIntVar $ getName var)
+  z3vars <- HM.fromList <$> mapM sequence [ (var, Z3VarName <$> mkFreshIntVar (getName var))
                                           | (var, TyBase RefinedBaseTy { baseType = TInt }) <- HM.toList args
                                           ]
   let z3args = HM.intersectionWith (,) args z3vars
 
   argsPresup <- genArgsPresup z3args
 
-  res <- mkFreshIntVar "_res$" -- TODO don't assume result : Int
+  res <- Z3VarName <$> mkFreshIntVar "_res$" -- TODO don't assume result : Int
   resConcl <- genRefinementCstrs z3args target res >>= mkAnd
 
   assert =<< argsPresup `mkImplies` resConcl
 
   convertZ3Result <$> check
 
-type ArgZ3Types = HM.HashMap VarName (Ty, AST)
+type ArgZ3Types = HM.HashMap VarName (Ty, Z3VarName)
 
 genArgsPresup :: ArgZ3Types -> Z3 AST
 genArgsPresup z3args = foldM addVar [] (HM.elems z3args) >>= mkAnd
@@ -51,11 +53,11 @@ genArgsPresup z3args = foldM addVar [] (HM.elems z3args) >>= mkAnd
     addVar cstrs (TyBase rbTy, z3var) = (cstrs <>) <$> genRefinementCstrs z3args rbTy z3var
     addVar cstrs _ = pure cstrs
 
-genRefinementCstrs :: ArgZ3Types -> RefinedBaseTy -> AST -> Z3 [AST]
-genRefinementCstrs args rbTy z3var
+genRefinementCstrs :: ArgZ3Types -> RefinedBaseTy -> Z3VarName -> Z3 [AST]
+genRefinementCstrs z3args rbTy z3var
   | not $ null conjs = do
     when (baseType rbTy /= TInt) $ error "Non-int refinements unsupported for now"
-    mapM (genCstr z3var) conjs
+    mapM (genCstr $ getZ3VarName z3var) conjs
   | otherwise = pure []
   where
     conjs = conjuncts $ baseTyRefinement rbTy
@@ -63,7 +65,7 @@ genRefinementCstrs args rbTy z3var
     genCstr v (AR op arg) = do
       z3arg <- case arg of
                     RArgZero -> mkInteger 0
-                    RArgVar var -> pure $ snd $ args HM.! var
+                    RArgVar var -> pure $ getZ3VarName $ snd $ z3args HM.! var
                     RArgVarLen _ -> error "TODO" -- TODO
       v `z3op` z3arg
       where
