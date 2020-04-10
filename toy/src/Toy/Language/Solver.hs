72d452a6773e75665029872595fee03f93681486
0xd34df00d <0xd34df00d@gmail.com>
2020-04-10 12:01:49 -0400

    Try living in MonadReader

--- toy/src/Toy/Language/Solver.hs
+++ toy/src/Toy/Language/Solver.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Toy.Language.Solver where
@@ -6,6 +7,7 @@ module Toy.Language.Solver where
 import qualified Data.HashMap.Strict as HM
 import Control.Arrow
 import Control.Monad
+import Control.Monad.Reader
 import Z3.Monad
 
 import Toy.Language.Syntax.Decls
@@ -18,7 +20,7 @@ newtype SolveContext = SolveContext
   } deriving (Eq, Ord, Show, Semigroup, Monoid)
 
 solve :: SolveContext -> FunSig -> FunDef -> IO SolveRes
-solve _ sig def = evalZ3 $ mkScript arg2type resType (funBody def)
+solve ctx sig def = evalZ3 $ mkScript ctx arg2type resType (funBody def)
   where
     (argTypes, resType) = splitTypes sig
     arg2type = HM.fromList $ zip (funArgs def) argTypes
@@ -27,23 +29,30 @@ type ArgTypes = HM.HashMap VarName Ty
 
 newtype Z3VarName = Z3VarName { getZ3VarName :: AST }
 
+data SolveEnvironment = SolveEnvironment
+  { ctx :: SolveContext
+  , z3args :: ArgZ3Types
+  }
+
 -- This expects that the pi-binders names in the type are equal to the argument names in the definition.
 -- TODO explicitly check for this.
-mkScript :: ArgTypes -> RefinedBaseTy -> Term -> Z3 SolveRes
-mkScript args target term = do
+mkScript :: SolveContext -> ArgTypes -> RefinedBaseTy -> Term -> Z3 SolveRes
+mkScript ctx args target term = do
   z3vars <- HM.fromList <$> mapM sequence [ (var, Z3VarName <$> mkFreshIntVar (getName var))
                                           | (var, TyBase RefinedBaseTy { baseType = TInt }) <- HM.toList args
                                           ]
   let z3args = HM.intersectionWith (,) args z3vars
+  let solveEnv = SolveEnvironment { .. }
 
-  argsPresup <- genArgsPresup z3args
+  flip runReaderT solveEnv $ do
+    argsPresup <- genArgsPresup
 
-  res <- Z3VarName <$> mkFreshIntVar "_res$" -- TODO don't assume result : Int
-  resConcl <- genRefinementCstrs z3args target res >>= mkAnd
+    res <- Z3VarName <$> mkFreshIntVar "_res$" -- TODO don't assume result : Int
+    resConcl <- genRefinementCstrs target res >>= mkAnd
 
-  assert =<< genTermsCstrs z3args res term
+    assert =<< genTermsCstrs res term
 
-  assert =<< mkNot =<< argsPresup `mkImplies` resConcl
+    assert =<< mkNot =<< argsPresup `mkImplies` resConcl
 
   --getModel >>= modelToString . fromJust . snd >>= liftIO . putStrLn
 
@@ -55,12 +64,12 @@ mkScript args target term = do
 
 type ArgZ3Types = HM.HashMap VarName (Ty, Z3VarName)
 
-genTermsCstrs :: ArgZ3Types -> Z3VarName -> Term -> Z3 AST
-genTermsCstrs z3args termVar (TBinOp t1 op t2) = do
+genTermsCstrs :: (MonadZ3 m, MonadReader SolveEnvironment m) => Z3VarName -> Term -> m AST
+genTermsCstrs termVar (TBinOp t1 op t2) = do
   t1var <- mkFreshIntVar "_linkVar_t1$"
   t2var <- mkFreshIntVar "_linkVar_t2$"
-  t1cstrs <- genTermsCstrs z3args (Z3VarName t1var) t1
-  t2cstrs <- genTermsCstrs z3args (Z3VarName t2var) t2
+  t1cstrs <- genTermsCstrs (Z3VarName t1var) t1
+  t2cstrs <- genTermsCstrs (Z3VarName t2var) t2
   bodyRes <- z3op t1var t2var
   bodyCstr <- getZ3VarName termVar `mkEq` bodyRes
   mkAnd [t1cstrs, t2cstrs, bodyCstr]
@@ -70,16 +79,20 @@ genTermsCstrs z3args termVar (TBinOp t1 op t2) = do
                 BinOpMinus -> \a b -> mkSub [a, b]
                 BinOpGt -> mkGt
                 BinOpLt -> mkLt
-genTermsCstrs z3args termVar (TName varName) = getZ3VarName termVar `mkEq` getZ3VarName (snd $ z3args HM.! varName)
+genTermsCstrs termVar (TName varName) = do
+  z3Var <- askZ3VarName varName
+  getZ3VarName termVar `mkEq` z3Var
 
-genArgsPresup :: ArgZ3Types -> Z3 AST
-genArgsPresup z3args = foldM addVar [] (HM.elems z3args) >>= mkAnd
+genArgsPresup :: (MonadZ3 m, MonadReader SolveEnvironment m) => m AST
+genArgsPresup = do
+  args <- asks $ HM.elems . z3args
+  foldM addVar [] args >>= mkAnd
   where
-    addVar cstrs (TyBase rbTy, z3var) = (cstrs <>) <$> genRefinementCstrs z3args rbTy z3var
+    addVar cstrs (TyBase rbTy, z3var) = (cstrs <>) <$> genRefinementCstrs rbTy z3var
     addVar cstrs _ = pure cstrs
 
-genRefinementCstrs :: ArgZ3Types -> RefinedBaseTy -> Z3VarName -> Z3 [AST]
-genRefinementCstrs z3args rbTy z3var
+genRefinementCstrs :: (MonadZ3 m, MonadReader SolveEnvironment m) => RefinedBaseTy -> Z3VarName -> m [AST]
+genRefinementCstrs rbTy z3var
   | not $ null conjs = do
     when (baseType rbTy /= TInt) $ error "Non-int refinements unsupported for now"
     mapM (genCstr $ getZ3VarName z3var) conjs
@@ -90,7 +103,7 @@ genRefinementCstrs z3args rbTy z3var
     genCstr v (AR op arg) = do
       z3arg <- case arg of
                     RArgZero -> mkInteger 0
-                    RArgVar var -> pure $ getZ3VarName $ snd $ z3args HM.! var
+                    RArgVar var -> askZ3VarName var
                     RArgVarLen _ -> error "TODO" -- TODO
       v `z3op` z3arg
       where
@@ -102,6 +115,9 @@ genRefinementCstrs z3args rbTy z3var
                     ROpGt -> mkGt
                     ROpGeq -> mkGe
 
+askZ3VarName :: MonadReader SolveEnvironment m => VarName -> m AST
+askZ3VarName var = getZ3VarName <$> asks (snd . (HM.! var) . z3args)
+
 convertZ3Result :: Result -> SolveRes
 convertZ3Result Sat = Correct
 convertZ3Result Unsat = Wrong
@@ -112,3 +128,7 @@ splitTypes = go . funTy
   where
     go (TyBase rbTy) = ([], rbTy)
     go (TyArrow ArrowTy { .. }) = first (domTy :) $ go codTy
+
+instance MonadZ3 m => MonadZ3 (ReaderT r m) where
+  getSolver = ReaderT $ const getSolver
+  getContext = ReaderT $ const getContext
