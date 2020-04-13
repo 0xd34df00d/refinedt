2b64a47b6ca95584bd29bdd65ec9b564f6d9e88c
0xd34df00d <0xd34df00d@gmail.com>
2020-04-13 13:02:24 -0400

    Derive Data for Ty representation types

--- toy/src/Toy/Language/Syntax/Types.hs
+++ toy/src/Toy/Language/Syntax/Types.hs
@@ -1,25 +1,27 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 module Toy.Language.Syntax.Types where
 
+import Data.Data
 import Data.Hashable
 import Data.String
 
-newtype VarName = VarName { getName :: String } deriving (Eq, Ord, Show, IsString, Hashable)
+newtype VarName = VarName { getName :: String } deriving (Eq, Ord, Show, IsString, Hashable, Data)
 
 data BaseTy = TBool | TInt | TIntList
-  deriving (Eq, Ord, Show, Enum, Bounded)
+  deriving (Eq, Ord, Show, Enum, Bounded, Data)
 
 -- Following liquid-types-08 paper, chapter 5
 
-data RefinementOp = ROpLt | ROpLeq | ROpEq | ROpNEq | ROpGt | ROpGeq deriving (Eq, Ord, Show, Enum, Bounded)
+data RefinementOp = ROpLt | ROpLeq | ROpEq | ROpNEq | ROpGt | ROpGeq deriving (Eq, Ord, Show, Enum, Bounded, Data)
 
-data RefinementArg = RArgZero | RArgVar VarName | RArgVarLen VarName deriving (Eq, Ord, Show)
+data RefinementArg = RArgZero | RArgVar VarName | RArgVarLen VarName deriving (Eq, Ord, Show, Data)
 
 data AtomicRefinement = AR RefinementOp RefinementArg
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Data)
 
-newtype Refinement = Refinement { conjuncts :: [AtomicRefinement] } deriving (Eq, Ord, Show)
+newtype Refinement = Refinement { conjuncts :: [AtomicRefinement] } deriving (Eq, Ord, Show, Data)
 
 trueRefinement :: Refinement
 trueRefinement = Refinement []
@@ -27,18 +29,18 @@ trueRefinement = Refinement []
 data RefinedBaseTy = RefinedBaseTy
  { baseType :: BaseTy
  , baseTyRefinement :: Refinement
- } deriving (Eq, Ord, Show)
+ } deriving (Eq, Ord, Show, Data)
 
 data ArrowTy = ArrowTy
  { piVarName :: Maybe VarName
  , domTy :: Ty
  , codTy :: Ty
- } deriving (Eq, Ord, Show)
+ } deriving (Eq, Ord, Show, Data)
 
 data Ty
   = TyArrow ArrowTy
   | TyBase RefinedBaseTy
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Data)
 
 stripRefinements :: Ty -> Ty
 stripRefinements = undefined
