7559cf986e048f77942d183ebde3400928cf4185
0xd34df00d <0xd34df00d@gmail.com>
2020-05-22 18:42:01 -0400

    Yay a very basic test passes

---
+++ toy/test/T30TermsCompilerSpec.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module T30TermsCompilerSpec(spec) where
+
+import Data.String.Interpolate
+import Test.Hspec
+
+import Idris.IdeModeClient
+import Toy.Language.Compiler
+import Toy.Language.Solver
+
+import TestUtils
+
+checkIdris :: String -> IdrisHandle -> Expectation
+checkIdris program ih = do
+  (ctx, (funSig, funDef)) <- testParseFunWithCtx program
+  res <- solve (buildCtx ctx) funSig funDef
+  res `shouldBe` Correct
+  runIdrisClient ih $ withFile $ \file -> do
+    mapM_ (write file . compileFunSig) ctx
+    write file $ compileFunSig funSig
+    write file $ compileFunDef funDef
+    testIdrisFile file
+
+spec :: Spec
+spec = testWithIdris $ do
+  describe "Terms with unrefined types" $ do
+    it "translates constants" $ checkIdris
+      [i|
+         someNum : Int
+         someNum = 42
+        |]
