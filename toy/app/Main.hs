dbae5081f44b05baba9ef5852c9a538cd67c412f
0xd34df00d <0xd34df00d@gmail.com>
2020-05-26 15:35:29 -0400

    Also, `Main` wasn't building for a while, so let's commit whatever I have

--- toy/app/Main.hs
+++ toy/app/Main.hs
@@ -1,21 +1,35 @@
 module Main where
 
+import Control.Monad.IO.Class
 import Data.Void
 import System.Environment
 import Text.Megaparsec hiding (State)
 
 import Toy.Language.Compiler
 import Toy.Language.Parser.Decl
-import Toy.Language.Syntax.Decl
+import Toy.Language.Syntax.Decls
 
-parseFunDecl :: String -> FunDecl
+import Z3.Monad
+
+parseFunDecl :: String -> FunSig
 parseFunDecl str = either (error . errorBundlePretty) id parsed
   where
-   parsed = runParser (funDecl <* eof) "" str :: Either (ParseErrorBundle String Void) FunDecl
+   parsed = runParser (funDecl <* eof) "" str :: Either (ParseErrorBundle String Void) FunSig
+
+script :: Int -> Z3 ()
+script n = do
+  v <- mkFreshIntVar "v"
+  zero <- mkInteger 0
+  n' <- mkInteger $ fromIntegral n
+  context <- sequence [v `mkGe` zero, v `mkLt` n']
+  assert =<< mkAnd context
+  check >>= liftIO . print
+  pure ()
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-       ("compileTy" : decls) -> mapM_ putStrLn $ compileFunDecl . parseFunDecl <$> decls
+       ("compileTy" : decls) -> mapM_ putStrLn $ compileFunSig . parseFunDecl <$> decls
+       ["z3", n] -> evalZ3 (script $ read n) >>= print
        _ -> putStrLn "Unrecognized arguments"
