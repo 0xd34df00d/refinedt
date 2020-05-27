24113fde3a75edc4b0b9907bf138a97e1dd55e2e
0xd34df00d <0xd34df00d@gmail.com>
2020-05-27 19:16:44 -0400

    Unwrap and rewrap funapps as needed
    Still doesn't work yet as Idris (obviously) cannot figure out what the
    predicate is. Looks like we need to translate the type as well.

--- toy/src/Toy/Language/Compiler.hs
+++ toy/src/Toy/Language/Compiler.hs
@@ -55,22 +55,33 @@ compileBaseTy TInt = "Int"
 compileBaseTy TIntList = "List Int"
 
 compileFunDef :: [FunSig] -> FunSig -> TypedFunDef -> String
-compileFunDef ctx sig def@FunDef { .. } = [i|#{funName} #{unwords funArgsNames} = #{wrapping resType $ compileTerm funBody}|]
+compileFunDef ctx sig def@FunDef { .. } = [i|#{funName} #{unwords funArgsNames} = #{funBodyStr}|]
   where
     funArgsNames = getName <$> funArgs
     resType = snd $ funTypesMapping sig def
+    funBodyStr = wrapping (TyBase resType) $ unwrapping (annotation funBody) $ compileTerm funBody
 
 compileTerm :: TypedTerm -> String
 compileTerm (TName _ var) = getName var
-compileTerm (TInteger _ n) = show n
+compileTerm (TInteger ty n) = wrapping ty $ show n
 compileTerm (TBinOp _ t1 op t2) = [i|(#{compileTerm t1} #{compileOp op} #{compileTerm t2})|]
-compileTerm (TApp _ t1 t2) = [i|#{parens t1} #{parens t2}|]
+compileTerm (TApp _ t1 t2)
+  | TyArrow ArrowTy { .. } <- annotation t1
+  , let t2str = wrapping domTy $ unwrapping (annotation t2) $ parens t2 = [i|#{parens t1} #{t2str}|]
+  | otherwise = error "Unexpected function type"
 compileTerm TIfThenElse { .. } = [i|if #{compileTerm tcond} then #{compileTerm tthen} else #{compileTerm telse}|]
 
-wrapping :: RefinedBaseTy -> String -> String
-wrapping RefinedBaseTy { .. } str
+wrapping :: Ty -> String -> String
+wrapping TyArrow {} str = str
+wrapping (TyBase RefinedBaseTy { .. }) str
   | baseTyRefinement == trueRefinement = str
   | otherwise = [i|(#{str} ** believe_me ())|]
+
+unwrapping :: Ty -> String -> String
+unwrapping TyArrow {} str = str
+unwrapping (TyBase RefinedBaseTy { .. }) str
+  | baseTyRefinement == trueRefinement = str
+  | otherwise = [i|(fst #{str})|]
 
 compileOp :: BinOp -> String
 compileOp = \case BinOpPlus -> "+"
