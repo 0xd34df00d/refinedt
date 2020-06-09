46d61b86ec2cc0e793cfb7de0d5d68bcd8fc9155
0xd34df00d <0xd34df00d@gmail.com>
2020-06-09 15:58:31 -0400

    Remove z3 cruft from main

--- toy/app/Main.hs
+++ toy/app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Control.Monad.IO.Class
 import Data.Void
 import System.Environment
 import Text.Megaparsec hiding (State)
@@ -9,27 +8,14 @@ import Toy.Language.Compiler
 import Toy.Language.Parser.Decl
 import Toy.Language.Syntax.Decls
 
-import Z3.Monad
-
 parseFunDecl :: String -> FunSig
 parseFunDecl str = either (error . errorBundlePretty) id parsed
   where
    parsed = runParser (funDecl <* eof) "" str :: Either (ParseErrorBundle String Void) FunSig
-
-script :: Int -> Z3 ()
-script n = do
-  v <- mkFreshIntVar "v"
-  zero <- mkInteger 0
-  n' <- mkInteger $ fromIntegral n
-  context <- sequence [v `mkGe` zero, v `mkLt` n']
-  assert =<< mkAnd context
-  check >>= liftIO . print
-  pure ()
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
        ("compileTy" : decls) -> mapM_ putStrLn $ compileFunSig . parseFunDecl <$> decls
-       ["z3", n] -> evalZ3 (script $ read n) >>= print
        _ -> putStrLn "Unrecognized arguments"
