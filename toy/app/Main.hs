ebf97c3296964f88366aa6c042f5ce3c6bdbe244
0xd34df00d <0xd34df00d@gmail.com>
2020-06-09 23:50:14 -0400

    Add a transpile command

--- toy/app/Main.hs
+++ toy/app/Main.hs
@@ -1,21 +1,26 @@
 module Main where
 
+import Data.Bifunctor
 import Data.Void
 import System.Environment
 import Text.Megaparsec hiding (State)
 
 import Toy.Language.Compiler
-import Toy.Language.Parser.Decl
+import Toy.Language.Parser
 import Toy.Language.Syntax.Decls
 
-parseFunDecl :: String -> FunSig
-parseFunDecl str = either (error . errorBundlePretty) id parsed
+parseFunDecls :: String -> Either String [Decl]
+parseFunDecls str = first errorBundlePretty parsed
   where
-   parsed = runParser (funSig <* eof) "" str :: Either (ParseErrorBundle String Void) FunSig
+   parsed = runParser (decls <* eof) "" str :: Either (ParseErrorBundle String Void) [Decl]
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-       ("compileTy" : decls) -> mapM_ putStrLn $ compileFunSig . parseFunDecl <$> decls
+       ["transpile", filename] -> do
+          contents <- readFile filename
+          case parseFunDecls contents of
+               Left err -> putStrLn $ "Parse error:\n" <> err
+               Right ds -> putStr $ compileDecls ds
        _ -> putStrLn "Unrecognized arguments"
