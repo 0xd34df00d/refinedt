4df2bf74d4401c8da9ca13b1bb8deb4cfda50674
0xd34df00d <0xd34df00d@gmail.com>
2020-06-14 14:35:05 -0400

    Just keep the constantly true refinement instead of Nothing

--- toy/src/Toy/Language/QueryGen.hs
+++ toy/src/Toy/Language/QueryGen.hs
@@ -6,18 +6,18 @@ import Toy.Language.Syntax.Types
 -- The intrinsic refinement characterizes the structure of the term and doesn't need to be checked but can be assumed.
 -- The VC proposition is whatever needs to hold for that specific term to type check (not including its subterms).
 data QAnnotation = QAnnotation
-  { intrinsicProp :: Maybe Refinement
+  { intrinsicProp :: Refinement
   , tyAnn :: Ty
   } deriving (Eq, Ord, Show)
 
 type QTerm = TermT QAnnotation
 type QFunDef = FunDefT QAnnotation
 
-emptyQAnn :: Maybe Refinement -> TypedTerm -> QTerm
+emptyQAnn :: Refinement -> TypedTerm -> QTerm
 emptyQAnn = fmap . QAnnotation
 
 genQueries :: TypedTerm -> QTerm
-genQueries t@(TName ty _) = emptyQAnn (tyRefinement ty) t
-genQueries (TInteger ty n) = TInteger (QAnnotation (Just refinement) ty) n
+genQueries t@(TName ty _) = emptyQAnn (tyRefinement' ty) t
+genQueries (TInteger ty n) = TInteger (QAnnotation refinement ty) n
   where
     refinement = Refinement [AR ROpEq $ RArgInt n]
