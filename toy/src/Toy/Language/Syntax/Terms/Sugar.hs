b83fc1e3968fc193444a5b45b61a585641bf9c89
0xd34df00d <0xd34df00d@gmail.com>
2020-06-15 15:05:34 -0400

    Add some syntactic sugar for terms manipulation

---
+++ toy/src/Toy/Language/Syntax/Terms/Sugar.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Toy.Language.Syntax.Terms.Sugar where
+
+import Data.String
+
+import Toy.Language.Syntax.Terms
+
+v :: IsString s => s
+v = "v"
+
+ti :: Int -> Term
+ti = TInteger ()
+
+(|=|), (|+|), (|-|), (|>|), (|<|), (|>=|), (|<=|) :: Term -> Term -> Term
+t1 |=| t2 = TBinOp () t1 BinOpEq t2
+t1 |+| t2 = TBinOp () t1 BinOpPlus t2
+t1 |-| t2 = TBinOp () t1 BinOpMinus t2
+t1 |>| t2 = TBinOp () t1 BinOpGt t2
+t1 |<| t2 = TBinOp () t1 BinOpLt t2
+t1 |>=| t2 = TBinOp () t1 BinOpGeq t2
+t1 |<=| t2 = TBinOp () t1 BinOpLeq t2
+
+len :: String -> Term
+len x = TApp () "len" (fromString x)
