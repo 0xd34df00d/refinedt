5efcc48890021183164bb1a1f3636b6ed3680e75
0xd34df00d <0xd34df00d@gmail.com>
2020-07-27 12:57:35 -0400

    Change the build to allow for tex filtering via ott

--- tex/build.sh
+++ tex/build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -o surface.ott.tex
+ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
 pdflatex paper.tex && bibtex paper.aux && pdflatex paper.tex && pdflatex paper.tex
