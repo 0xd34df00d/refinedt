085c916e51e967685657cad5936d71d47fd9f109
0xd34df00d <0xd34df00d@gmail.com>
2020-09-16 19:26:58 -0400

    Update build.sh

--- tex/build.sh
+++ tex/build.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-set -e
-
 ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
 pdflatex paper.tex
 bibtex paper.aux
