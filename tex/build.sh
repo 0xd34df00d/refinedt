83ded4c6d3e60bd51ad7f4106f6d9abbef08488c
0xd34df00d <0xd34df00d@gmail.com>
2020-09-21 10:52:08 -0500

    Use xelatex to build the pdf in CI

--- tex/build.sh
+++ tex/build.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
-pdflatex paper.tex
+xelatex paper.tex
 bibtex paper.aux
-pdflatex paper.tex
-pdflatex paper.tex
+xelatex paper.tex
+xelatex paper.tex
 exit 0
