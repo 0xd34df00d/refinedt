3f05933a19348d9195de5022a908714d292ec036
0xd34df00d <0xd34df00d@gmail.com>
2020-11-04 12:36:14 -0600

    Use pdflatex in build scripts too

--- tex/comp.sh
+++ tex/comp.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
+set -e
+
 ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
-pdflatex paper.tex
 bibtex paper.aux
-pdflatex paper.tex
-pdflatex paper.tex
-exit 0
+pdflatex -shell-escape paper.tex
