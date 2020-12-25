15d7817f3c8827c2b5cd3bc13bc33128bea2bb31
0xd34df00d <0xd34df00d@gmail.com>
2020-12-24 19:04:20 -0600

    Build with -shell-escape in the CI as well

--- tex/build.sh
+++ tex/build.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
-pdflatex paper.tex
+pdflatex -shell-escape paper.tex
 bibtex paper.aux
-pdflatex paper.tex
-pdflatex paper.tex
+pdflatex -shell-escape paper.tex
+pdflatex -shell-escape paper.tex
 exit 0
