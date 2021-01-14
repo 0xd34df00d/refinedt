2af8b9fba92d57829e42fc05bf224f1f1c6c15e7
0xd34df00d <0xd34df00d@gmail.com>
2021-01-14 14:32:13 -0600

    Use xelatex again

--- tex/build.sh
+++ tex/build.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
-pdflatex -shell-escape paper.tex
+xelatex -shell-escape paper.tex
 bibtex paper.aux
-pdflatex -shell-escape paper.tex
-pdflatex -shell-escape paper.tex
+xelatex -shell-escape paper.tex
+xelatex -shell-escape paper.tex
 exit 0
--- tex/comp.sh
+++ tex/comp.sh
@@ -4,4 +4,4 @@ set -e
 
 ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
 bibtex paper.aux
-pdflatex -shell-escape paper.tex
+xelatex -shell-escape paper.tex
