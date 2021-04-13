ba15f6d14b28b55b8c2c469c942e05bbc0905abb
0xd34df00d <0xd34df00d@gmail.com>
2021-04-13 10:27:30 -0500

    Build everything with pdflatex

--- tex/build.sh
+++ tex/build.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
-xelatex -shell-escape paper.tex
+pdflatex -shell-escape paper.tex
 bibtex paper.aux
-xelatex -shell-escape paper.tex
-xelatex -shell-escape paper.tex
+pdflatex -shell-escape paper.tex
+pdflatex -shell-escape paper.tex
 exit 0
--- tex/comp.sh
+++ tex/comp.sh
@@ -4,4 +4,4 @@ set -e
 
 ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
 bibtex paper.aux
-xelatex -shell-escape paper.tex
+pdflatex -shell-escape paper.tex
