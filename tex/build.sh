6ed001f2fdacf46813d687b8bafb9dfa4c215d47
0xd34df00d <0xd34df00d@gmail.com>
2021-07-09 21:04:14 -0500

    Remove the shell scripts

--- tex/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
-pdflatex -shell-escape paper.tex
-bibtex paper.aux
-pdflatex -shell-escape paper.tex
-pdflatex -shell-escape paper.tex
-exit 0
--- tex/comp.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-set -e
-
-ott -tex_wrap false -i common.ott -i surface.ott -i core.ott -i translation.ott -o surface.ott.tex -tex_filter paper.mng paper.tex
-pdflatex -shell-escape paper.tex
-bibtex paper.aux
